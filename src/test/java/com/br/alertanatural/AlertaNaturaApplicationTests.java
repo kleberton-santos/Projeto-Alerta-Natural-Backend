6f81f39ce96a5ad5c3719eff8905f67c5d62b705
kleberton-santos <ti.klebersantos@gmail.com>
2025-01-05 22:00:23 -0300

    Criação do projeto

---
+++ src/test/java/com/br/alertanatural/AlertaNaturaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.alertanatural;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlertaNaturaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
