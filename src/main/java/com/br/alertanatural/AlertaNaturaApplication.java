48474320a1d8b9f008075a34ea90dfc5547a1033
kleberton-santos <ti.klebersantos@gmail.com>
2025-01-05 22:00:23 -0300

    Criação do projeto

---
+++ src/main/java/com/br/alertanatural/AlertaNaturaApplication.java
@@ -0,0 +1,13 @@
+package com.br.alertanatural;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlertaNaturaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlertaNaturaApplication.class, args);
+	}
+
+}
