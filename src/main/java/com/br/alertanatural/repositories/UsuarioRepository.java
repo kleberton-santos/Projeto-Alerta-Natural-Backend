2a6df9145e828a5ce5bb13f4f64a3b4ff4bf6654
kleberton-santos <ti.klebersantos@gmail.com>
2025-01-18 18:23:42 -0300

    criação de DTO para login

--- src/main/java/com/br/alertanatural/repositories/UsuarioRepository.java
+++ src/main/java/com/br/alertanatural/repositories/UsuarioRepository.java
@@ -3,5 +3,9 @@ package com.br.alertanatural.repositories;
 import com.br.alertanatural.models.Usuarios;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UsuarioRepository extends JpaRepository<Usuarios,Long> {
+
+    Optional<Usuarios> findByEmail(String email);
 }
--- src/main/java/com/br/alertanatural/services/UsuarioService.java
+++ src/main/java/com/br/alertanatural/services/UsuarioService.java
@@ -1,5 +1,6 @@
 package com.br.alertanatural.services;
 
+import com.br.alertanatural.DTOs.LoginDTO;
 import com.br.alertanatural.models.Usuarios;
 import com.br.alertanatural.repositories.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class UsuarioService {
@@ -14,8 +16,43 @@ public class UsuarioService {
     @Autowired
     private UsuarioRepository usuarioRepository;
 
+    public Usuarios criarLogin(LoginDTO loginDTO) {
+        Optional<Usuarios> usuarioOptional = usuarioRepository.findByEmail(loginDTO.getEmail());
+
+        if (usuarioOptional.isPresent()) {
+            Usuarios usuario = usuarioOptional.get();
+            if (loginDTO.getSenha().equals(usuario.getSenha())) {
+                return usuario;
+            } else {
+                throw new RuntimeException("Senha incorreta");
+            }
+        } else {
+            throw new RuntimeException("Usuário não encontrado");
+        }
+    }
+
+    public List<LoginDTO> listarLoging() {
+        List<Usuarios> usuarios = usuarioRepository.findAll();
+
+        // Convertendo a lista de Usuários para uma lista de LoginDTO
+        return usuarios.stream()
+                .map(usuario -> new LoginDTO(usuario.getEmail(), null)) // Senha é null pois não deve ser exposta
+                .collect(Collectors.toList());
+    }
+
     public Usuarios criarUsuario(Usuarios usuarios){
         return usuarioRepository.save(usuarios);
+    }
+
+    public LoginDTO listarLoginPorId(Long id) {
+        Optional<Usuarios> usuarioOptional = usuarioRepository.findById(id);
+
+        if (usuarioOptional.isPresent()) {
+            Usuarios usuario = usuarioOptional.get();
+            return new LoginDTO(usuario.getEmail(), null); // Senha é null
+        } else {
+            throw new RuntimeException("Usuário não encontrado");
+        }
     }
 
     public List<Usuarios> listarUsuarios(){
