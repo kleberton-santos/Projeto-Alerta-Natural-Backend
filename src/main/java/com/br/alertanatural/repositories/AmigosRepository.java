e9e1e924048a452de05cdb401b912b7d1cb24dcf
kleberton-santos <ti.klebersantos@gmail.com>
2025-02-19 20:55:32 -0300

    endpoint de deletar seguidor

--- src/main/java/com/br/alertanatural/repositories/AmigosRepository.java
+++ src/main/java/com/br/alertanatural/repositories/AmigosRepository.java
@@ -1,7 +1,12 @@
 package com.br.alertanatural.repositories;
 
 import com.br.alertanatural.models.Amigos;
+import com.br.alertanatural.models.Usuarios;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
@@ -9,4 +14,14 @@ import java.util.Optional;
 public interface AmigosRepository extends JpaRepository<Amigos, Long> {
     List<Amigos> findByUsuarioIdusuario(Long idUsuario);
     Optional<Amigos> findById(Long idAmigo);
+    boolean existsByUsuarioIdusuarioAndAmigoIdusuario(Long idUsuario, Long idAmigoUsuario);
+
+
+    // Remover uma amizade
+    @Modifying
+    @Transactional
+    @Query("DELETE FROM Amigos a WHERE a.usuario.id = :usuarioId AND a.amigo.id = :amigoId")
+    void deleteByUsuarioIdAndAmigoId(@Param("usuarioId") Long usuarioId, @Param("amigoId") Long amigoId);
+
+
 }
