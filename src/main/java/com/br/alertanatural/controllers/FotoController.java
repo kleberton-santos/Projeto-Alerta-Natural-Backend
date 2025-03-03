d2af625839a71f1a7386765a9c00f2684c17c9b4
kleberton-santos <ti.klebersantos@gmail.com>
2025-03-03 15:04:28 -0300

    enpoint de foto padrao

--- src/main/java/com/br/alertanatural/controllers/FotoController.java
+++ src/main/java/com/br/alertanatural/controllers/FotoController.java
@@ -7,12 +7,22 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import org.springframework.core.io.Resource;
+import org.springframework.http.ResponseEntity;
+import org.springframework.core.io.UrlResource;
+import org.springframework.http.MediaType;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 import java.util.List;
 
 @RestController
 @RequestMapping("/fotos")
 @Tag(name = "Fotos", description = "Endpoints relacionados às fotos")
 public class FotoController {
+
+    // Caminho da imagem padrão
+    private static final String UPLOAD_DIR = "C:/foto-alerta";
 
     @Autowired
     private FotoService fotoService;
@@ -36,5 +46,27 @@ public class FotoController {
     @GetMapping("/publicacao/{idPublicacao}")
     public List<FotosDTO> listarFotosPorPublicacao(@PathVariable Long idPublicacao) {
         return fotoService.listarFotosPorPublicacao(idPublicacao);
+    }
+
+    // Endpoint para servir a imagem padrão
+    @Operation(summary = "Obter imagem padrão", description = "Retorna a imagem padrão (icon_user.png)")
+    @GetMapping("/padrao")
+    public ResponseEntity<Resource> getImagemPadrao() {
+        try {
+            // Define o caminho da imagem padrão
+            Path caminhoImagem = Paths.get(UPLOAD_DIR).resolve("icon_user.png");
+            Resource resource = new UrlResource(caminhoImagem.toUri());
+
+            // Verifica se a imagem existe e é legível
+            if (resource.exists() || resource.isReadable()) {
+                return ResponseEntity.ok()
+                        .contentType(MediaType.IMAGE_PNG) // Define o tipo de conteúdo como PNG
+                        .body(resource);
+            } else {
+                throw new RuntimeException("Imagem padrão não encontrada");
+            }
+        } catch (Exception e) {
+            throw new RuntimeException("Erro ao carregar a imagem padrão", e);
+        }
     }
 }
